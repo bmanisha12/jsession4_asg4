e5a35a038c99d341a39974053fb3130525468acd
bmanisha12 <b.manisha59@gmail.com>
2016-03-24 00:36:47 +0530

    Added files via upload

---
+++ strrev.java
@@ -0,0 +1,16 @@
+
+public class strrev {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer str = new StringBuffer("Mercedes");
+		System.out.println("Reverse string is : "+str.reverse());
+		
+
+	}
+
+}
